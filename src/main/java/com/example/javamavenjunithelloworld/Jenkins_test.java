2d78e1c0a82291eb1b7c2e683b2f54bb20c78cba
Kasigaribharath <kasigaribharath2002@gmail.com>
2023-10-15 11:03:27 +0530

    Jenkins_test.java file is added

---
+++ src/main/java/com/example/javamavenjunithelloworld/Jenkins_test.java
@@ -0,0 +1,7 @@
+public class Jenkins_test
+{
+    public static void main(String[] args)
+    {
+        System.out.println("This program is to test jenkins server if it is pulling if changes are commited here");
+    }
+}
